e6d67382b9540d368f60bf9c3deee42f4601b7fb
Munjal A Patel <2717686+munjalpatel@users.noreply.github.com>
2025-05-01 08:37:37 -0700

    test(grasql): move test resolver out of the main module

--- lib/grasql.ex
+++ lib/grasql.ex
@@ -218,17 +218,3 @@ defmodule GraSQL do
     end
   end
 end
-
-# Define SimpleResolver module for doctests
-defmodule GraSQL.SimpleResolver do
-  @moduledoc """
-  A simple implementation of the GraSQL.SchemaResolver behaviour for use in doctests.
-  This resolver simply returns the input values without transformation.
-  """
-
-  @behaviour GraSQL.SchemaResolver
-
-  def resolve_table(table, _ctx), do: table
-
-  def resolve_relationship(rel, _ctx), do: rel
-end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,3 @@
 ExUnit.start()
+# Add test/support directory to the code path for tests
+Code.require_file("support/simple_resolver.ex", __DIR__)
